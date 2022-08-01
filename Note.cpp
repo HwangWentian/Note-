c64fb45e189d4c41d6e5705fb34e1dae2720b8c2
HwangWentian <hwt2009@foxmail.com>
2022-08-01 11:53:37 +0800

    first commit

---
+++ Note.cpp
@@ -0,0 +1,22 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+class Note
+{
+public:
+    string notes[1000];
+    int quantityOfNotes;
+    void addANote();
+    void list();
+};
+
+void Note::addANote()
+{
+    quantityOfNotes
+}
+
+int main()
+{
+
+    return 0;
+}
