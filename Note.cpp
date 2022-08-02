352ae492babf143fd7ae9761202caf5ef8eb9487
HwangWentian <hwt2009@foxmail.com>
2022-08-02 19:51:29 +0800

    Added ability to get input.

--- Note.cpp
+++ Note.cpp
@@ -1,6 +1,27 @@
 #include <bits/stdc++.h>
 using namespace std;
 
+string getInput()
+{
+    string Input, line;
+    bool mark = true;
+    int lineLength;
+    cout << "Type \"eND~\" to end typing.\n";
+    do
+    {
+        cin >> line;
+        lineLength = line.size();
+        if (lineLength >= 4 && line.substr(lineLength - 4) == "eND~")
+        {
+            Input += line.substr(0, lineLength - 4);
+            mark = false;
+        }
+        else
+            Input += line;
+    } while (mark);
+    return Input;
+}
+
 class Note
 {
 public:
@@ -18,7 +39,13 @@ Note::Note(void)
 
 void Note::addOneNote()
 {
-    quantityOfNotes += 1;
+    if(quantityOfNotes < 1000)
+    {
+        notes[quantityOfNotes] = getInput();
+        quantityOfNotes += 1;
+    }
+    else
+        cout << "\aYour notebook is full, please delete some useless ones.";
 }
 
 int main()
