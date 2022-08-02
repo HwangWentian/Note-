1c890181acbacb8f27b094fedcfebe76114763e7
HwangWentian <hwt2009@foxmail.com>
2022-08-02 17:15:09 +0800

    adfg

--- Note.cpp
+++ Note.cpp
@@ -4,29 +4,25 @@ using namespace std;
 class Note
 {
 public:
+    Note();
     string notes[1000];
     int quantityOfNotes;
-    void addANote();
-    void list();
+    void addOneNote();
+    void listRecentNotes();
 };
 
 Note::Note(void)
 {
     quantityOfNotes = 0;
-    for (int i = 0; i < 1000; i++)
-    {
-        notes[i] = NULL;
-    }
-    cout << "success";
 }
 
-void Note::addANote()
+void Note::addOneNote()
 {
     quantityOfNotes += 1;
 }
 
 int main()
 {
-
+    Note myNote;
     return 0;
 }
