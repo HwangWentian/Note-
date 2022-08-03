dad0260f2ac6f05b8968f80cab6e5a5d3d32adf5
HwangWentian <hwt2009@foxmail.com>
2022-08-03 19:55:43 +0800

    Still no progress

--- Note.cpp
+++ Note.cpp
@@ -1,6 +1,6 @@
 #include <bits/stdc++.h>
 using namespace std;
-
+int a = 1;
 string getInput()
 {
     string Input, line;
@@ -22,11 +22,19 @@ string getInput()
     return Input;
 }
 
+string getTime()
+{
+    time_t t = time(0);
+    char tmp[32] = {NULL};
+    strftime(tmp, sizeof(tmp), "%Y-%m-%d %H:%M:%S", localtime(&t));
+    return tmp;
+}
+
 class Note
 {
 public:
     Note();
-    string notes[1000];
+    string notes[1000][2];
     int quantityOfNotes;
     void addOneNote();
     void listRecentNotes();
@@ -39,17 +47,44 @@ Note::Note(void)
 
 void Note::addOneNote()
 {
-    if(quantityOfNotes < 1000)
+    if (quantityOfNotes < 1000)
     {
-        notes[quantityOfNotes] = getInput();
+        notes[quantityOfNotes][0] = getInput();
+        notes[quantityOfNotes][1] = getTime();
         quantityOfNotes += 1;
     }
     else
         cout << "\aYour notebook is full, please delete some useless ones.";
 }
 
+void Note::listRecentNotes()
+{
+    if (!quantityOfNotes)
+    {
+        cout << "\aYou don't have any notes yet.";
+        return;
+    }
+    else if (quantityOfNotes <= 5)
+    {
+        for (int i = 0; i < quantityOfNotes; i++)
+        {
+            cout << i + 1 << notes[quantityOfNotes][0] << endl;
+            cout << "_-_-_-_-_-_-_-_-_" << notes[quantityOfNotes][1] << endl;
+        }
+    }
+    else
+    {
+        for (int i = 0; i < 5; i++)
+        {
+            cout << i + 1 << notes[quantityOfNotes][0] << endl;
+            cout << "_-_-_-_-_-_-_-_-_" << notes[quantityOfNotes][1] << endl;
+        }
+    }
+}
+
 int main()
 {
+    cout<<a;
     Note myNote;
     return 0;
 }
